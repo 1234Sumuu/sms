580659814484033ed54516af00b7012fc1b7314c
1234Sumuu <sumaiya.akter.cse@ulab.edu.bd>
2021-11-03 09:46:03 -0700

    laravel sms

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table-> vigIncrements('id');
+            $table->string('cne');
+            $table->string('firstName');
+            $table->string('secondName');
+            $table->string('age');
+            $table->string('speciality');  
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
